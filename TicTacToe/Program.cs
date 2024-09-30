84c393532d7656bd4d680529c9e08d4f054fb755
LEGM2002 <luiseduardogarciamartinez92@gmail.com>
2024-09-30 01:47:46 -0600

    Push to github

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections;
+
+namespace TicTacToe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Clear(); // mantener el color en toda la consola
+            Menu();
+        }
+
+        // menu principal
+        public static void Menu()
+        {
+            int opcion = 0;
+            bool opcionValida = true;
+            do
+            {
+                Console.WriteLine("- - - - TIC TAC TOE - - - -" +
+                    "\n[1]. Jugar." +
+                    "\n[2]. Salir.");
+                Console.Write("Ingrese la opción elegida: ");
+                try
+                {
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Formato erróneo, ingrese un número...\n");
+                }
+
+                if (opcion == 1)
+                {
+                    bool jugarDeNuevo = true;
+                    while (jugarDeNuevo)
+                    {
+                        Juego();
+                        jugarDeNuevo = Validacion();
+                        if (!jugarDeNuevo)
+                        {
+                            break;
+                        }
+                    }
+                }
+                else if (opcion == 2)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Valor incorrecto, inténtelo de nuevo...\n");
+                    opcionValida = false;
+                }
+            }
+            while (!opcionValida);
+
+            return;
+        }
+
+        // metodo encargado de la logica del juego
+        public static void Juego()
+        {
+            Tablero tablero = new Tablero();    // instanciar un tablero cada que inicie un juego
+            int jugador = 2;
+            int contadorDeTurnos = 0;
+            int casillaSeleccionada = 0;
+            bool casillaValida = true;
+            do
+            {
+                tablero.CrearTablero();
+
+                // verificar ganador antes de cambiar turno
+                if (tablero.ExisteGanador())
+                {
+                    Console.WriteLine("\nFelicidades jugador {0}, haz ganado :)", jugador);
+                    break;
+                }
+
+                // verificar empate
+                if (contadorDeTurnos == 9)
+                {
+                    Console.WriteLine("\n\tJuego empatado :|");
+                    break;
+                }
+
+                //cambio de turno
+                if (jugador == 2) { jugador = 1; }
+                else if (jugador == 1) { jugador = 2; }
+
+                do
+                {
+                    Console.Write("\nJugador {0}, elija una casilla: ", jugador);
+                    try
+                    {
+                        casillaSeleccionada = Convert.ToInt32(Console.ReadLine());  // casteo
+                    }
+                    catch (Exception)
+                    {
+                        Console.Clear();
+                        tablero.CrearTablero();
+                        Console.WriteLine("\nFormato erróneo, ingrese un número...");
+                    }
+
+                    casillaValida = tablero.CasillaDisponible(casillaSeleccionada);   // verificar que este disponible la casilla
+                    
+                    if (casillaValida)
+                    {
+                        tablero.ColocarXoO(jugador, casillaSeleccionada);
+                        contadorDeTurnos++;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        tablero.CrearTablero();
+                        Console.WriteLine("\nValor incorrecto, inténtelo de nuevo...");
+                    }
+                }
+                while (!casillaValida);
+            }
+            while (true);
+        }
+
+        // metodo para verificar si quiere volver a jugar
+        public static bool Validacion()
+        {
+            int opcion = 0;
+            bool opcionValida = true;
+            bool formatoValido = true;
+            Console.WriteLine("\nPresione cualquier tecla para continuar....");
+            Console.ReadKey();
+            do
+            {
+                Console.Clear();
+                if (!formatoValido || !opcionValida)
+                {
+                    Console.WriteLine("Valor incorrecto, inténtelo de nuevo...\n");
+                }
+                Console.WriteLine("¿Desea jugar de nuevo?" +
+                "\n[1]. Si." +
+                "\n[2]. No.");
+                Console.Write("Ingrese la opción elegida: ");
+                try
+                {
+                    opcion = Convert.ToInt32(Console.ReadLine());
+                    formatoValido = true;
+                }
+                catch (Exception)
+                {
+                    Console.Clear();
+                    formatoValido = false;
+                }
+
+                if (opcion == 1)
+                {
+                    return true;
+                }
+                else if (opcion == 2)
+                {
+                    break;
+                }
+                else
+                {
+                    opcionValida = false;
+                }
+            }
+            while (!opcionValida);
+
+            return false;
+        }
+    }
+}
---
+++ TicTacToe/Tablero.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    internal class Tablero
+    {
+        private char[,] tablero =
+        {
+            {'1', '2', '3' },
+            {'4', '5', '6' },
+            {'7', '8', '9' },
+        };
+
+        // metodo para crear el tablero de tic tac toe con su respectivo numero de casilla
+        public void CrearTablero()
+        {
+            Console.Clear(); // limpiar pantalla para que se muestre el tablero actualizado
+            Console.WriteLine("\t     |     |     ");
+            Console.WriteLine("\t  {0}  |  {1}  |  {2}  ", tablero[0, 0], tablero[0, 1], tablero[0, 2]);
+            Console.WriteLine("\t_____|_____|_____");
+            Console.WriteLine("\t     |     |     ");
+            Console.WriteLine("\t  {0}  |  {1}  |  {2}  ", tablero[1, 0], tablero[1, 1], tablero[1, 2]);
+            Console.WriteLine("\t_____|_____|_____");
+            Console.WriteLine("\t     |     |     ");
+            Console.WriteLine("\t  {0}  |  {1}  |  {2}  ", tablero[2, 0], tablero[2, 1], tablero[2, 2]);
+            Console.WriteLine("\t     |     |     ");
+        }
+
+        // metodo para verificar si esta disponible la casilla
+        public bool CasillaDisponible(int casillaSeleccionada)
+        {
+            if ((casillaSeleccionada == 1 && tablero[0, 0] == '1') ||
+                (casillaSeleccionada == 2 && tablero[0, 1] == '2') ||
+                (casillaSeleccionada == 3 && tablero[0, 2] == '3') ||
+                (casillaSeleccionada == 4 && tablero[1, 0] == '4') ||
+                (casillaSeleccionada == 5 && tablero[1, 1] == '5') ||
+                (casillaSeleccionada == 6 && tablero[1, 2] == '6') ||
+                (casillaSeleccionada == 7 && tablero[2, 0] == '7') ||
+                (casillaSeleccionada == 8 && tablero[2, 1] == '8') ||
+                (casillaSeleccionada == 9 && tablero[2, 2] == '9'))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // metodo para identificar jugador
+        public void ColocarXoO(int jugador, int casillaSeleccionada)
+        {
+            char signo = ' ';
+            if (jugador == 1) { signo = 'X'; }
+            else if (jugador == 2) { signo = 'O'; }
+
+            switch (casillaSeleccionada)
+            {
+                case 1: tablero[0, 0] = signo; break;
+                case 2: tablero[0, 1] = signo; break;
+                case 3: tablero[0, 2] = signo; break;
+                case 4: tablero[1, 0] = signo; break;
+                case 5: tablero[1, 1] = signo; break;
+                case 6: tablero[1, 2] = signo; break;
+                case 7: tablero[2, 0] = signo; break;
+                case 8: tablero[2, 1] = signo; break;
+                case 9: tablero[2, 2] = signo; break;
+            }
+        }
+
+        // metodo para verificar si hay un ganador
+        public bool ExisteGanador()
+        {
+            char[] signos = { 'X', 'O' };
+            foreach (char signo in signos)
+            {
+                // Verificar filas y columnas
+                for (int i = 0; i < 3; i++)
+                {
+                    if ((tablero[i, 0] == signo && tablero[i, 1] == signo && tablero[i, 2] == signo) ||  // filas
+                        (tablero[0, i] == signo && tablero[1, i] == signo && tablero[2, i] == signo))    // columnas
+                    {
+                        return true;
+                    }
+                }
+
+                // Verificar diagonales
+                if ((tablero[0, 0] == signo && tablero[1, 1] == signo && tablero[2, 2] == signo) ||  // diagonal principal
+                    (tablero[0, 2] == signo && tablero[1, 1] == signo && tablero[2, 0] == signo))    // diagonal secundaria
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
